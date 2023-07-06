47a0e89a55f91865aced2c1f2fe89b5ae23d84d8
P-Contreras <palo.contreras@live.com>
2023-07-06 15:58:11 -0300

    funcion js para botones de texto e imagen

---
+++ script.js
@@ -0,0 +1,31 @@
+//boton de texto e imagen - asides
+const botonImagen = document.getElementById('boton-imagen');
+const botonTexto = document.getElementById('boton-texto');
+
+const asideImagen = document.getElementById('asideImagen');
+const asideTexto = document.getElementById('asideTexto');
+
+//eventlistener botones: texto - imagen
+
+botonImagen.addEventListener('click', () => {
+    showAside(asideImagen);
+});
+botonTexto.addEventListener('click', () => {
+    showAside(asideTexto);
+});
+
+// funciones aside-botones
+
+const showAside = (asideToShow) => {
+    if (asideToShow === asideTexto) {
+    asideTexto.classList.remove('hidden');
+    asideImagen.classList.add('hidden');
+    } else {
+    asideImagen.classList.remove('hidden');
+    asideTexto.classList.add('hidden');
+    }
+};
+
+
+
+
